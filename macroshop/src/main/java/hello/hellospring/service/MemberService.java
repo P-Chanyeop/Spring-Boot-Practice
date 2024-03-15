18b5acf7a6f2c09205cbf52119766f9aa3921d59
P-Chanyeop <qkrcksduq1gh@naver.com>
2024-03-15 15:47:38 +0900

    changed files

--- macroshop/src/main/java/hello/hellospring/service/MemberService.java
+++ macroshop/src/main/java/hello/hellospring/service/MemberService.java
@@ -15,14 +15,14 @@ public class MemberService {
     public Long join(Member member) {
         // 같은 이름의 중복 회원 X
         // command + option + v 입력시 표현식 optinal 자동완성!! <<중요>>
+        // 이미 findByName함수가 Optinal로 반환하기 때문에, Optinal<Memeber> result는 지워도된다.
         Optional<Member> result = memberRepository.findByName(member.getName());
-        result.isPresent(m -> {
-            try {
-                System.out.println("hello");
-            } catch (IllegalStateException) {
-                throw new IllegalStateException("이미 존재하는 회원입니다.");
-            }
-        });
+
+        // isPresent함수는 값이 있음, 없음을 체크하는 함수. Optinal 타입일 경우만 가능하다.
+        // orElseGet같은 함수도 사용가능.
+        if (result.isPresent()){
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
 
         memberRepository.save(member);
         return member.getId();
